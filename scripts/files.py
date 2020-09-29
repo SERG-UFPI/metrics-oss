b666ed84da8ac4c7990fe1621af0366114d67026
Lucas Hiago <lucashiago63@gmail.com>
2020-09-28 21:51:06 -0300

    Feat: add misc for I/O operations

---
+++ scripts/files.py
@@ -0,0 +1,12 @@
+import json
+
+def save_file(data, filename):
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+def read_from_file(filename):
+    try:
+        with open(filename, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
