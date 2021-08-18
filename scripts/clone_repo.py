c42ca08c773965bc1a7196b50de8fd6e2d297f99
Lucas Hiago <lucashiago63@gmail.com>
2021-08-17 21:52:38 -0300

    feat: add perceval github command

--- scripts/clone_repo.py
+++ scripts/clone_repo.py
@@ -9,14 +9,17 @@ def clone_repo(owner, repository, repo_id):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    command = "perceval git https://github.com/{0}/{1}.git --git-path {2}.git > {2}.json".format(
-        owner, repository, path
+    command_git = (
+        f"perceval git https://github.com/{owner}/{repository}.git "
+        f"--git-path {path}.git > {path}-git.json"
     )
+    command_github = f"perceval github {owner} {repository} -o {path}-github.json"
 
     error = ""
 
     try:
-        os.system(command)
+        os.system(command_git)
+        os.system(command_github)
     except Exception as e:
         error = str(e)
 
