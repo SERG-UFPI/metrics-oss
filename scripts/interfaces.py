ea22f049df7eb3df78ef6b54d7635843a6e145c3
Carlos Eduardo <carloseduardoifpi@gmail.com>
2021-08-29 23:24:49 -0300

    chore: add interface file with author dataclass

---
+++ scripts/interfaces.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class Author:
+    name: str
+    commits: int
+    projects: int
+    added_lines: int
+    removed_lines: int
+    avg_files: float
